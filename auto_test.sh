e4e6066bf5172bf8ad2a40ba87e62ef43805ff94
loken <1964329685@qq.om>
2016-07-24 22:56:06 +0800

    导入项目

---
+++ auto_test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#bin
+./OCR_recognition ./images/A.jpg
+./OCR_recognition ./images/B.jpg
+./OCR_recognition ./images/C.jpg
+./OCR_recognition ./images/D.jpg
+./OCR_recognition ./images/E.jpg
+./OCR_recognition ./images/F.jpg
+./OCR_recognition ./images/G.jpg
+./OCR_recognition ./images/H.jpg
+./OCR_recognition ./images/I.jpg
